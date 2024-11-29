94ac627d2260e1189ec00bea9b274ab4ed8fe390
Harun Cakiroglu <hac005@edu.zealand.dk>
2024-11-29 13:03:03 +0100

    ændret props

--- ClassLibrary/Models/Medlem.cs
+++ ClassLibrary/Models/Medlem.cs
@@ -3,16 +3,15 @@
 public class Medlem
 {
 public string Navn { get; set; }
-public string Efternavn { get; set; }
 
 public string TelefonNummer {  get; set; }
 
-
+public string Email { get; set; }
  
-public Medlem (string navn, string efternavn, string telefonNummer)
+public Medlem (string navn, string telefonNummer, string email)
     {
         Navn = navn;
-        Efternavn = efternavn;
+        Email = email;
         TelefonNummer = telefonNummer;
     }
 }
