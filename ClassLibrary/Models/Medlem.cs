b38108605436190eca4e0ecb1a8a0eb1cdbab7cd
Harun Cakiroglu <hac005@edu.zealand.dk>
2024-11-29 13:00:16 +0100

    Construcer og properties

--- ClassLibrary/Models/Medlem.cs
+++ ClassLibrary/Models/Medlem.cs
@@ -2,5 +2,17 @@
 
 public class Medlem
 {
+public string Navn { get; set; }
+public string Efternavn { get; set; }
 
+public string TelefonNummer {  get; set; }
+
+
+ 
+public Medlem (string navn, string efternavn, string telefonNummer)
+    {
+        Navn = navn;
+        Efternavn = efternavn;
+        TelefonNummer = telefonNummer;
+    }
 }
