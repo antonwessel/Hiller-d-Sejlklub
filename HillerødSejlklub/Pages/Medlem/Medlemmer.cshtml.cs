2f4d838c2c7c81ad3a2028f63ff89d8e7432cd7a
antonwessel <wessel.anton@outlook.com>
2024-11-29 13:05:32 +0100

    Tilføj Medlemmer Page

---
+++ HillerødSejlklub/Pages/Medlem/Medlemmer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødSejlklub.Pages.Medlem
+{
+    public class MedlemmerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
