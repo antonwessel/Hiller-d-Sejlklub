55f1561441963cb556676d58a501fa629fe35100
antonwessel <wessel.anton@outlook.com>
2024-12-01 12:27:53 +0100

    Slet en båd muligt

---
+++ HillerødSejlklub/Pages/Både/SletBåd.cshtml.cs
@@ -0,0 +1,31 @@
+using ClassLibrary.Interfaces;
+using ClassLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødSejlklub.Pages.Både;
+
+public class SletBådModel : PageModel
+{
+    private IBådService _bådService;
+
+    [BindProperty]
+    public Båd Båd { get; set; }
+
+    public SletBådModel(IBådService bådService)
+    {
+        _bådService = bådService;
+    }
+
+    public IActionResult OnGet(string navn)
+    {
+        Båd = _bådService.GetBåd(navn);
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        _bådService.DeleteBåd(Båd.Navn);
+        return RedirectToPage("AlleBåde");
+    }
+}
