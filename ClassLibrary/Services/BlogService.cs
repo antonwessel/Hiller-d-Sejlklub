e79f1d7da23eaa517ef741910e913082b00a6b5d
Harun Cakiroglu <hac005@edu.zealand.dk>
2024-12-02 12:30:52 +0100

    Tilføj blog

--- ClassLibrary/Services/BlogService.cs
+++ ClassLibrary/Services/BlogService.cs
@@ -1,29 +1,36 @@
 ﻿using ClassLibrary.Interfaces;
+using ClassLibrary.MockData;
 using ClassLibrary.Models;
 
 namespace ClassLibrary.Services;
 
 public class BlogService : IBlogService
 {
-    public void AddBlog(Blog blog)
-    {
-        throw new NotImplementedException();
-    }
+	private List<Blog> _blogListe;
 
-    public Blog DeleteBlog(string? titel)
-    {
-        throw new NotImplementedException();
-    }
+	public BlogService()
+	{
+		_blogListe = MockBlog.GetBlogsAsList();
+	}
+	public void AddBlog(Blog blog)
+	{
+		_blogListe.Add(blog);
+	}
 
-    public Blog GetBlog(string titel)
-    {
-        throw new NotImplementedException();
-    }
+	public Blog DeleteBlog(string? titel)
+	{
+		throw new NotImplementedException();
+	}
 
-    public List<Blog> GetBlogs() => MockData.MockBlog.GetBlogsAsList();
+	public Blog GetBlog(string titel)
+	{
+		throw new NotImplementedException();
+	}
 
-    public void UpdateBlog(Blog blog)
-    {
-        throw new NotImplementedException();
-    }
+	public List<Blog> GetBlogs() => MockData.MockBlog.GetBlogsAsList();
+
+	public void UpdateBlog(Blog blog)
+	{
+		throw new NotImplementedException();
+	}
 }
