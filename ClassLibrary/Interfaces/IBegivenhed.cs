a22d5a3c649cc979fbcbf4809af7da49832e78f8
Mahamed Ibrahim <somalim4300@outlook.dk>
2024-12-02 11:38:10 +0100

    Interfacet er lavet færdig

--- ClassLibrary/Interfaces/IBegivenhed.cs
+++ ClassLibrary/Interfaces/IBegivenhed.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace ClassLibrary.Interfaces
 {
     internal interface IBegivenhed
     {
+        List<Begivenhed> GetBegivenhed();
+        void AddBegivenhed(Begivenhed begivenhed);
+        void UpdateBegivenhed(Begivenhed begivenhed);
+        Begivenhed GetBegivenhed(string navn);
+        Begivenhed DeleteBegivenhed(string navn);
+
     }
 }
