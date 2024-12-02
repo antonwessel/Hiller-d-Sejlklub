20dc29ad3766a63b9af9ad8ef1abd8cecce413aa
Harun Cakiroglu <hac005@edu.zealand.dk>
2024-12-02 13:10:34 +0100

    Tilføjet requriements

--- ClassLibrary/Models/Blog.cs
+++ ClassLibrary/Models/Blog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,17 @@ namespace ClassLibrary.Models
 {
     public class Blog
     {
+        [Required(ErrorMessage = "Titlen er påkrævet.")]
+    [StringLength(100, ErrorMessage = "Titel må højst være 100 tegn.")]
         public string BlogTitel { get; set; }
+
         public DateTime BlogDato { get; set; }
+        [Required(ErrorMessage = "Navn er påkrævet.")]
+        [StringLength(50, ErrorMessage = "Navn må højst være 50 tegn.")]
         public string BlogForfatter { get; set; }
+
+        [Required(ErrorMessage = "Indhold er påkrævet.")]
+        [StringLength(5000, ErrorMessage = "Indhold må højst være 5000 tegn.")]
         public string BlogIndhold { get; set; }
 
         public Blog(string blogTitel, string blogForfatter, string blogIndhold)
