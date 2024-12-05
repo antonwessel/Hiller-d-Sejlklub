d638d4a96379eb541a0b4aa920081a53e5fd3c12
antonwessel <wessel.anton@outlook.com>
2024-12-05 14:48:54 +0100

    Man kan nu logge ind som admin

---
+++ ClassLibrary/Helpers/AdminState.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibrary.Helpers;
+
+/// <summary>
+/// Holder styr på, om en admin er logget ind, og viser eller skjuler CRUD-funktioner baseret på det.
+/// </summary>
+public static class AdminState
+{
+    private static readonly string _username = "admin";
+    private static readonly string _password = "1234";
+    public static bool IsAdminLoggedIn { get; private set; }
+
+    public static bool AdminLogin(string username, string password)
+    {
+        if (username == _username && password == _password)
+        {
+            IsAdminLoggedIn = true;
+            return true;
+        }
+        return false;
+    }
+
+    public static void Logout()
+    {
+        IsAdminLoggedIn = false;
+    }
+}
