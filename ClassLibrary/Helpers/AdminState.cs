efa68dee6cae74057e6b599d8e989c37c124ba08
antonwessel <wessel.anton@outlook.com>
2024-12-09 00:40:53 +0100

    IsAdminLoggedIn sat til false by default

--- ClassLibrary/Helpers/AdminState.cs
+++ ClassLibrary/Helpers/AdminState.cs
@@ -1,13 +1,13 @@
 ﻿namespace ClassLibrary.Helpers;
 
 /// <summary>
-/// Holder styr på, om en admin er logget ind, og viser eller skjuler CRUD-funktioner baseret på det.
+/// Holder styr på, om en admin er logget ind. IsAdminLoggedIn viser eller skjuler CRUD-funktioner baseret på det.
 /// </summary>
 public static class AdminState
 {
     private static readonly string _username = "admin";
     private static readonly string _password = "1234";
-    public static bool IsAdminLoggedIn { get; private set; } = true;
+    public static bool IsAdminLoggedIn { get; private set; }
 
     public static bool AdminLogin(string username, string password)
     {
