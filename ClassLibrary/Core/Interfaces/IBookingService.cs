50f1d02d4126d6c150888872064f655943e465c2
antonwessel <wessel.anton@outlook.com>
2024-12-10 17:18:58 +0100

    refactor and fix small errors

---
+++ ClassLibrary/Core/Interfaces/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using ClassLibrary.Core.Models;
+
+namespace ClassLibrary.Core.Interfaces;
+
+public interface IBookingService
+{
+    void AddBooking(Båd boat, Medlem medlem);
+    void UpdateBooking(Booking booking);
+    Booking DeleteBooking(Guid id);
+}
---
+++ ClassLibrary/Core/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibrary.Core.Models;
+
+public class Booking
+{
+    public Guid Id { get; set; }
+    public DateTime DateBooked { get; set; }
+    public Medlem MedlemToBook { get; set; }
+    public Båd BoatToBook { get; set; }
+
+    public Booking(DateTime date, Medlem medlem, Båd boat)
+    {
+        Id = Guid.NewGuid();
+        DateBooked = date;
+        MedlemToBook = medlem;
+        BoatToBook = boat;
+    }
+}
