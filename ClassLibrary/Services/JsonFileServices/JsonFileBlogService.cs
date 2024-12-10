2481570dfad4ef922f000197a84c5a811399117c
antonwessel <wessel.anton@outlook.com>
2024-12-10 17:32:43 +0100

    make files

---
+++ ClassLibrary/Services/JsonFileServices/JsonFileBlogService.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary.Core.Interfaces;
+using ClassLibrary.Core.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ClassLibrary.Services.JsonFileServices;
+
+public class JsonFileBlogService : IJsonDataService<Blog>
+{
+    public string FilePath { get; }
+
+    public JsonFileBlogService(IWebHostEnvironment webHostEnvironment)
+    {
+        FilePath = Path.Combine(webHostEnvironment.WebRootPath, "data", "blogs.json");
+    }
+
+    public IEnumerable<Blog> LoadData()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveData(IEnumerable<Blog> data)
+    {
+        throw new NotImplementedException();
+    }
+}
