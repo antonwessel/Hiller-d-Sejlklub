023db97648a1f9d18aac28b3b42e436607fc0b75
Abdi Hassan <Abdimalik2002@outlook.dk>
2024-12-10 15:24:28 +0100

    add metoder i interface

---
+++ ClassLibrary/Interfaces/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Interfaces
+{
+    interface IBookingService
+    {
+        void AddBooking(Båd båd, Medlem medlem);
+        void UpdateBooking(Booking booking);
+        Booking DeleteBooking(string navn);
+    }
+}
