8ec38e9a0cd299a9dcebfc29d2efc24116d0f3c4
Abdi Hassan <Abdimalik2002@outlook.dk>
2024-12-10 14:59:35 +0100

    tilføjelse af noget mockdata til booking

---
+++ ClassLibrary/MockData/MockBooking.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.MockData
+{
+    public class MockBooking
+    {
+        private static readonly List<Booking> _bookings = new()
+        {
+            new Booking(
+                Guid.NewGuid(),
+                DateTime.Now.AddDays(-2),
+                MockMedlem.GetMembersAsList()[0], // Første medlem
+                MockBåd.GetBoatsAsList()[0]      // Første båd
+            ),
+            new Booking(
+                Guid.NewGuid(),
+                DateTime.Now.AddDays(1),
+                MockMedlem.GetMembersAsList()[1], // Andet medlem
+                MockBåd.GetBoatsAsList()[1]      // Anden båd
+            )
+        };
+
+        public static List<Booking> GetBookingsAsList() => _bookings;
+    }
+}
---
+++ ClassLibrary/Models/Booking.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary.MockData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    public class Booking
+    {
+        private Guid guid;
+        private DateTime dateTime;
+        private Medlem medlem;
+        private Båd båd;
+
+        public Booking(Guid guid, DateTime dateTime, Medlem medlem, Båd båd)
+        {
+            this.guid = guid;
+            this.dateTime = dateTime;
+            this.medlem = medlem;
+            this.båd = båd;
+        }
+
+        
+        
+    }
+}
