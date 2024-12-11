bf877b229e0c337d3c0268392d5fb3bef0270260
antonwessel <wessel.anton@outlook.com>
2024-12-11 17:53:51 +0100

    boat data persistence

---
+++ ClassLibrary/Services/JsonFileServices/JsonFileBoatService.cs
@@ -0,0 +1,36 @@
+﻿using ClassLibrary.Core.Interfaces;
+using ClassLibrary.Core.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Text.Json;
+
+namespace ClassLibrary.Services.JsonFileServices;
+
+public class JsonFileBoatService : IJsonDataService<Båd>
+{
+    public string FilePath { get; }
+
+    public JsonFileBoatService(IWebHostEnvironment webHostEnvironment)
+    {
+        FilePath = Path.Combine(webHostEnvironment.WebRootPath, "data", "boats.json");
+    }
+
+    public IEnumerable<Båd> LoadData()
+    {
+        if (!File.Exists(FilePath))
+        {
+            return [];
+        }
+
+        var json = File.ReadAllText(FilePath);
+        return JsonSerializer.Deserialize<IEnumerable<Båd>>(json) ?? [];
+    }
+
+    public void SaveData(IEnumerable<Båd> data)
+    {
+        var json = JsonSerializer.Serialize(data, new JsonSerializerOptions
+        {
+            WriteIndented = true,
+        });
+        File.WriteAllText(FilePath, json);
+    }
+}
