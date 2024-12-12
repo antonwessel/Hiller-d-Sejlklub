c08d751bbcaa41b2cecbabc89fc4f67a2d8216a8
antonwessel <wessel.anton@outlook.com>
2024-12-12 11:00:14 +0100

    fremvisning af navn

--- HillerødSejlklub/Pages/Både/Booking/AllBookings.cshtml.cs
+++ HillerødSejlklub/Pages/Både/Booking/AllBookings.cshtml.cs
@@ -8,16 +8,22 @@ public class AllBookingsModel : PageModel
 {
     private IBookingService _bookingService;
 
+    [BindProperty]
+    public List<ClassLibrary.Core.Models.Booking> Bookings { get; set; }
+
+    [BindProperty]
+    public string CurrentBoatName { get; set; }
+
     public AllBookingsModel(IBookingService bookingService)
     {
         _bookingService = bookingService;
     }
 
-    [BindProperty]
-    public List<ClassLibrary.Core.Models.Booking> Bookings { get; set; } = [];
+
 
     public IActionResult OnGet(string bådNavn)
     {
+        CurrentBoatName = bådNavn;
         Bookings = _bookingService.GetAllBookings(bådNavn);
         return Page();
     }
